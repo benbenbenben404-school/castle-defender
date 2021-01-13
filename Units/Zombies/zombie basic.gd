2d2f1068e0a97a81698b6be5e74f6cafcd04f5df
benbenbenben404-school <73062112+benbenbenben404-school@users.noreply.github.com>
2021-01-13 15:41:29 +0000

    Zombie Walk animaiotn

--- Units/Zombies/zombie basic.gd
+++ Units/Zombies/zombie basic.gd
@@ -1,3 +1,4 @@
+
 extends KinematicBody
 
 
@@ -6,9 +7,10 @@ extends KinematicBody
 # var b: String = "text"
 var health = 1
 var damage = 1
-var speed = 10
-
-
+var speed = 1
+var velocity
+export var anim_speed = 1
+onready var anim_tree = $"AnimationTree"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -16,5 +18,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if !$"Forward Check".get_overlapping_areas():
-		move_and_slide(Vector3(1,0,0)*speed)
+	#if !$"Forward Check".get_overlapping_areas():
+	velocity = anim_tree.get_root_motion_transform()
+	move_and_slide(velocity.origin*110)
+	#print(velocity.origin*100)
+	#anim_tree.advance(delta*10)
+	#anim_tree.tree_root.animation = "Run"
+	#anim_tree.tree_root.play()
+
+#plz do not fly up thx
